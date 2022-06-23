c63100e91b978aac95313c52669cb4207fffb168
aoyetibo557 <aoyetibo@gmail.com>
2022-06-23 09:47:06 -0400

    Updated app.js. added the not found route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => (
+  <div>
+    <h2>404 - Not Found!</h2>
+    <Link to="/">Go Home</Link>
+  </div>
+);
 
 
 function App() {
@@ -16,7 +24,11 @@ function App() {
             <Home />
             <Footer />
          </Route>
-       
+
+          <Route component={NotFound}>
+
+          </Route>
+
        </Switch>
      </Router>
     </div>
